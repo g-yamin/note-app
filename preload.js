4b5520d4229e3c8947a8afc81f09edce078d62aa
g-yamin <gym1404401304@gmail.com>
2024-06-07 14:38:40 +0800

    note 框架搭建一

---
+++ preload.js
@@ -0,0 +1,24 @@
+// preload.js
+
+// 所有的 Node.js API接口 都可以在 preload 进程中被调用.
+// 它拥有与Chrome扩展一样的沙盒。
+
+import { ipcRenderer } from 'electron';
+
+contextBridge.exposeInMainWorld('versions', {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron,
+  ping: () => ipcRenderer.invoke('ping')
+})
+
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const dependency of ['chrome', 'node', 'electron']) {
+    replaceText(`${dependency}-version`, process.versions[dependency])
+  }
+})
